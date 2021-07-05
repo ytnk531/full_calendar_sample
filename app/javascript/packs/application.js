ffe9ca14df4e00b810f1228da43fbfece5ecc55e
Yudai Tanaka <ytnk531@gmail.com>
2021-07-06 07:04:02 +0900

    js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../src/js'
+// import '../src/js'
 
 Rails.start()
 Turbolinks.start()
