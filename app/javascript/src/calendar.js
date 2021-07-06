f4e5eb97ab1c814dbd8d83b06bb06c956ed37d77
Yudai Tanaka <ytnk531@gmail.com>
2021-07-06 10:32:30 +0900

    enable webpacker version

--- app/javascript/src/calendar.js
+++ app/javascript/src/calendar.js
@@ -3,6 +3,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 document.addEventListener('turbolinks:load', function() {
         const calendarEl = document.getElementById('webpacker-calendar')
+        if(!calendarEl) { return }
         const calendar = new Calendar(calendarEl, {
                 plugins: [ dayGridPlugin ],
                 initialView: 'dayGridMonth'
